7c112636d611f648202c799454f1a68d05483249
Richard Forrester <richard.forrester@gmail.com>
2017-11-03 16:07:19 -0600

    added react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
 import './App.scss';
+import { Grid, Jumbotron, Button } from 'react-bootstrap';
 import Header from "./Header";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header/>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <div>
+           <Header/>
+            <Jumbotron>
+                <Grid>
+                    <h1>Welcome to React</h1>
+                    <p>
+                        <Button
+                            bsStyle="success"
+                            bsSize="large"
+                            href="http://react-bootstrap.github.io/components.html"
+                            target="_blank">
+                            View React Bootstrap Docs
+                        </Button>
+                    </p>
+                </Grid>
+            </Jumbotron>
+            <Videos></Videos>
+        </div>
     );
   }
 }
--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import './App.scss';
+import { Grid, Navbar } from 'react-bootstrap';
+
 
 class Header extends Component {
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
+                <Navbar className="bg-inverse" inverse fixedTop >
+                    <Grid>
+                        <Navbar.Header>
+                            <Navbar.Brand>
+                                <a href="/">React App</a>
+                            </Navbar.Brand>
+                        </Navbar.Header>
+                    </Grid>
+                </Navbar>
             </div>
         );
     }
